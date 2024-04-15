1e3981d3a7460ee2c4bb611013fd88dc42158016
sonpipe <pipefc2003@gmail.com>
2024-04-15 10:59:27 -0300

    disenado sistema preeliminar de semaforos

--- arduinoSemaphore/main/main.ino
+++ arduinoSemaphore/main/main.ino
@@ -1,9 +1,25 @@
 
-const int PIN_GREEN = 33;
+const int PIN_R_2 = 15;
+const int PIN_Y_2 = 2;
+const int PIN_G_2 = 12;
+const int PIN_R_1 = 35;
+const int PIN_Y_1 = 23;
+const int PIN_G_1 = 21;
+
+
+
+
+
+
 void setup() {
   Serial.begin(115200);
   
-  pinMode(PIN_GREEN,OUTPUT);
+  pinMode(PIN_G_2,OUTPUT);
+  pinMode(PIN_Y_2,OUTPUT);
+  pinMode(PIN_R_2,OUTPUT);
+  pinMode(PIN_R_1,OUTPUT);
+  pinMode(PIN_Y_1,OUTPUT);
+  pinMode(PIN_G_1,OUTPUT);
 }
 
 // Aquí configuramos lo que debe hacer cuando recibe un valor.
@@ -12,14 +28,34 @@ void setup() {
 void loop() {
 
     
-  digitalWrite(PIN_GREEN,HIGH);
-  delay(3000);
-  digitalWrite(PIN_GREEN,LOW);
-  delay(3000);
+  semaphore1Loop();
+  semaphore2Loop();
   
 }
 
 
+void semaphore1Loop(){
 
+  digitalWrite(PIN_R_1,HIGH);
+  delay(5000);
+  digitalWrite(PIN_R_1,LOW);
+  digitalWrite(PIN_Y_1,HIGH);
+  delay(1500);
+  digitalWrite(PIN_Y_1,LOW);
+  digitalWrite(PIN_G_1,HIGH);
+  delay(7000);
+  digitalWrite(PIN_G_1,LOW);
+}
 
+void semaphore2Loop(){
 
+  digitalWrite(PIN_R_2,HIGH);
+  delay(5000);
+  digitalWrite(PIN_R_2,LOW);
+  digitalWrite(PIN_Y_2,HIGH);
+  delay(1500);
+  digitalWrite(PIN_Y_2,LOW);
+  digitalWrite(PIN_G_2,HIGH);
+  delay(7000);
+  digitalWrite(PIN_G_2,LOW);
+}
